6cfbf1481b45670ad008290224502a17bd077fe0
Ripley Bobko <rbobko08@gmail.com>
2026-09-19 13:22:52 -0700

    do not use

--- Assets/Scripts/Level_Manager.cs
+++ Assets/Scripts/Level_Manager.cs
@@ -3,9 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class Level_Manager : MonoBehaviour
 {
+    public Animator vignette_animator;
     private int current_level;
+    void Start()
+    {
+        LoadNextLevel();
+    }
     void LoadNextLevel()
     {
-        
+        //vignette_animator.Play("CloseLevel");
+        vignette_animator.StopPlayback();
     }
 }
