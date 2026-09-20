bb3bf0805945b7c08fd6d16ca2fc82f6295bf884
Noah <noahhoblet@hotmail.com>
2026-09-19 19:00:32 -0700

    Update SharedData.cs

--- Assets/Scripts/SharedData.cs
+++ Assets/Scripts/SharedData.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [System.Serializable]
 public class SharedData : MonoBehaviour
@@ -8,26 +9,26 @@ public class SharedData : MonoBehaviour
     public enum GameAudioType
     {
         Music = 0,
-        Player,
         SoundEffects,
-        NumberOfGameAudioTypes
+        Voicelines,
     };
     [HideInInspector] public static List<float> volumes;  // GameAudioType -> float
     public static GameAudioType gameAudioType;
-
+    public static float masterVolume;
 
     void Awake()
     {
         SharedData.volumes = new List<float>();
-        // VOLUME
-        for (int i = 0; i < (int)GameAudioType.NumberOfGameAudioTypes; i++)
-        {
-            SharedData.volumes.Add(1.0f);            
-        }
 
+        // VOLUME
+        for (int i = 0; i < Enum.GetNames(typeof(GameAudioType)).Length; i++)
+        {
+            SharedData.volumes.Add(1.0f);
+        }
     }
 
-    public void ChangeVolume(){
+    public void ChangeVolume(int volumeIdx, float volume){
         //GameAudioType.
+        SharedData.volumes[volumeIdx] = volume;
     }
 }
