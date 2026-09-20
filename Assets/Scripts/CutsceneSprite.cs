32220c47638b6857468dca877fd7ce84a8a7bdf6
Gabriel Ramirez <gabekramirez@gmail.com>
2026-09-20 00:38:33 -0700

    2nd Cutscene

---
+++ Assets/Scripts/CutsceneSprite.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class CutsceneSprite : MonoBehaviour
+{
+    [SerializeField] private List<Sprite> sprites;
+    private Vector2 startPosition;
+    private Vector2 targetPosition;
+    private float time = 1.0f;
+    private float animationTime = 1.0f;
+    private SpriteRenderer spriteRenderer;
+
+    public void ChangeSprite(int index)
+    {
+        spriteRenderer.sprite = sprites[index];
+    }
+
+    public void Flip()
+    {
+        spriteRenderer.flipX = true;
+    }
+
+    public void Unflip()
+    {
+        spriteRenderer.flipX = false;
+    }
+
+    public void Show()
+    {
+        spriteRenderer.enabled = true;
+    }
+
+    public void Hide()
+    {
+        spriteRenderer.enabled = false;
+    }
+
+    public void GoTo(Vector2 goPosition, float goTime)
+    {
+        startPosition = transform.position;
+        targetPosition = goPosition;
+        time = 0.0f;
+        animationTime = goTime;
+        if (animationTime == 0.0f)
+        {
+            transform.position = goPosition;
+        }
+    }
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        targetPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (animationTime != 0.0f)
+        {
+            time += Time.deltaTime;
+            if (sprites.Count > 0)
+            {
+                int frame = (int)(time * 10) % sprites.Count;
+                if (time >= animationTime)
+                {
+                    time = animationTime;
+                    frame = 0;
+                }
+                ChangeSprite(frame);
+            }
+            transform.position = Vector2.Lerp(startPosition, targetPosition, time / animationTime);
+        }
+    }
+}
